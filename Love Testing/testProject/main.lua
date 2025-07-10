10bf3cdd5fbf3b5cc28b77ebf4f3305a37e377ee
zabloingg <harrisonbradley1806@gmail.com>
2025-07-10 22:08:26 +1000

    Update main.lua

--- Love Testing/testProject/main.lua
+++ Love Testing/testProject/main.lua
@@ -12,8 +12,12 @@ end
 
 function love.draw()
     love.graphics.print(number)
+    love.graphics.setColor(204/255, 102/255, 255/255)
 end
 
+-- setColor follows the structure (Red, Green, Blue, Alpha) 
+-- we divide by 255 to convert from 0-255 to 0-1 to fit the love format
+-- (32 byte colour depth to 1 byte colour depth)
 
 -- EXAMPLE 2
 
