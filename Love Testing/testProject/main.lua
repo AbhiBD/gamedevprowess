0ddc13b01378596aa13b7db7a1e02bed29c28628
zabloingg <harrisonbradley1806@gmail.com>
2025-07-10 23:14:26 +1000

    Update main.lua

--- Love Testing/testProject/main.lua
+++ Love Testing/testProject/main.lua
@@ -38,7 +38,7 @@ function love.update(dt)
 end
 
 function love.draw()
-    love.graphics.setColor(1,0,0, 0.2)
+    love.graphics.setColor(1,0,0, 0.7)
     love.graphics.circle('fill', target.x, target.y, target.radius)
 
 
