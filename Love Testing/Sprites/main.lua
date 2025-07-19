32ce915984a0b570dbcdde5b380e9c7f12131146
zabloingg <harrisonbradley1806@gmail.com>
2025-07-19 22:29:45 +1000

    dsf

---
+++ Love Testing/Sprites/main.lua
@@ -0,0 +1,29 @@
+function love.load()
+    player = {}
+    player.x = 400
+    player.y = 200
+    player.speed = 5
+    player.sprite = love.graphics.newImage('Sprites/annie.png')
+    background = love.graphics.newImage('Sprites/leaguemap.png')
+end
+
+function love.update(dt)
+
+    if love.keyboard.isDown("d") then
+        player.x = player.x + player.speed
+    end
+    if love.keyboard.isDown("a") then
+        player.x = player.x - player.speed
+    end
+    if love.keyboard.isDown("w") then
+        player.y = player.y - player.speed
+    end
+    if love.keyboard.isDown("s") then
+        player.y = player.y + player.speed
+    end
+end
+
+function love.draw()
+    love.graphics.draw(background, 0, 0)
+    love.graphics.draw(player.sprite, player.x, player.y)
+end
