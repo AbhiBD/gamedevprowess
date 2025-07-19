d2bf9910191d45d07d78e88276a5c63c0368a394
AbhiBD <abhineet.banerjee01@gmail.com>
2025-07-19 19:58:43 +0530

    billiejean

--- Love Testing/Character Animations/main.lua
+++ Love Testing/Character Animations/main.lua
@@ -1,33 +1,68 @@
 function love.load()
+    
+    anim8 = require 'libraries/anim8'
+    love.graphics.setDefaultFilter('nearest', 'nearest')
+    
     player = {}
     player.x = 400
     player.y = 200
-    player.speed = 5
+    player.speed = 2
     player.sprite = love.graphics.newImage('sprites/char.png')
-    background = love.graphics.newImage('sprites/background.png')
+    player.spritesheet = love.graphics.newImage('sprites/spritesheet1.png')
+    player.grid = anim8.newGrid(68,72, player.spritesheet:getWidth(), player.spritesheet:getHeight())
+
+    player.animations = {}
+    player.animations.down = anim8.newAnimation(player.grid('1-4', 1), 0.15)
+    player.animations.left = anim8.newAnimation(player.grid('1-4', 2), 0.15)
+    player.animations.right = anim8.newAnimation(player.grid('1-4', 3), 0.15)
+    player.animations.up = anim8.newAnimation(player.grid('1-4', 4), 0.15)
+
+    background = love.graphics.newImage('sprites/pallet.png')
     backgroundproperties = {}
-    backgroundproperties.scale = 6.3
+    backgroundproperties.scale = 3.25
+
+    player.state = player.animations.down
+ 
 end
 
 function love.update(dt)
 
+    local isMoving = false
+
     if love.keyboard.isDown("d") then
         player.x = player.x + player.speed
+        player.animations.right:update(dt)
+        player.state = player.animations.right
+        isMoving = true
     end
     if love.keyboard.isDown("a") then
         player.x = player.x - player.speed
+        player.animations.left:update(dt)
+        player.state = player.animations.left
+        isMoving = true
     end
     if love.keyboard.isDown("w") then
         player.y = player.y - player.speed
+        player.animations.up:update(dt)
+        player.state = player.animations.up
+        isMoving = true
     end
     if love.keyboard.isDown("s") then
         player.y = player.y + player.speed
+        player.animations.down:update(dt)
+        player.state = player.animations.down
+        isMoving = true
     end
+
+    if isMoving == false then
+        player.state:gotoFrame(1)
+    end
+    
 end
 
 function love.draw()
-    love.graphics.scale(backgroundproperties.scale, backgroundproperties.scale)
-    love.graphics.draw(background,0,0)
-    love.graphics.scale(1/backgroundproperties.scale,1/backgroundproperties.scale)
-    love.graphics.draw(player.sprite, player.x, player.y)
+    love.graphics.draw(background,0,0,nil, backgroundproperties.scale)
+
+    player.state:draw(player.spritesheet, player.x,player.y, nil, 2)
+    
 end
